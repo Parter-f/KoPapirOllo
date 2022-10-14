da2b7999d8f3196f90e539d239d22c998cda5bc9
Árpás Péter <arpaspeter0@gmail.com>
2022-10-14 09:31:03 +0200

    Kő click kész

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -3,12 +3,109 @@ package com.example.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView dontetlenszoveg;
+    private ImageView gepvalasztas;
+    private ImageView playervalasztas;
+    private ImageView gephp1;
+    private ImageView gephp2;
+    private ImageView gephp3;
+    private ImageView playerhp1;
+    private ImageView playerhp2;
+    private ImageView playerhp3;
+    private ImageView valasztasko;
+    private ImageView valasztaspapir;
+    private ImageView valasztasollo;
+    private Random rnd = new Random();
+    private int playerValNum;
+    private int gepValNum;
+    private int dontetlenNum;
+    private int gephpNum;
+    private int playerhpNum;
+
+
+private void init(){
+    dontetlenszoveg = findViewById(R.id.dontetlenszoveg);
+    gepvalasztas = findViewById(R.id.gepvalasztas);
+    playervalasztas = findViewById(R.id.playervalasztas);
+    gephp1 = findViewById(R.id.gephp1);
+    gephp2 = findViewById(R.id.gephp2);
+    gephp3 = findViewById(R.id.gephp3);
+    playerhp1 = findViewById(R.id.playerhp1);
+    playerhp2 = findViewById(R.id.playerhp2);
+    playerhp3 = findViewById(R.id.playerhp3);
+    valasztasko = findViewById(R.id.valasztasko);
+    valasztaspapir = findViewById(R.id.valasztaspapir);
+    valasztasollo = findViewById(R.id.valasztasollo);
+    rnd = new Random();
+    playerValNum = 0;
+    gepValNum = 0;
+    dontetlenNum = 0;
+    gephpNum = 3;
+    playerhpNum = 3;
+
+
+
+}
+private void koclick(){
+
+    playervalasztas.setBackgroundResource(R.drawable.rock);
+    playerValNum = 0;
+    gepValNum = rnd.nextInt(3);
+    if(gepValNum == 0){
+        gepvalasztas.setBackgroundResource(R.drawable.rock);
+        dontetlenNum++;
+    }
+    if(gepValNum == 1){
+        gepvalasztas.setBackgroundResource(R.drawable.paper);
+        playerhpNum--;
+        if(playerhpNum == 2){
+            playerhp1.setBackgroundResource(R.drawable.heart1);
+        }
+        if(playerhpNum == 1){
+            playerhp2.setBackgroundResource(R.drawable.heart1);
+        }
+        if(playerhpNum == 0){
+            playerhp3.setBackgroundResource(R.drawable.heart1);
+            //vesztettél
+        }
+
+    }
+    if(gephpNum == 2){
+        gepvalasztas.setBackgroundResource(R.drawable.scissors);
+        gephpNum--;
+        if(gephpNum == 2){
+            gephp1.setBackgroundResource(R.drawable.heart1);
+        }
+        if(gephpNum == 1){
+            gephp2.setBackgroundResource(R.drawable.heart1);
+        }
+        if(gephpNum == 0){
+            gephp3.setBackgroundResource(R.drawable.heart1);
+            //nyertél
+        }
+    }
+
+
+
+}
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+
+
+
     }
 }
